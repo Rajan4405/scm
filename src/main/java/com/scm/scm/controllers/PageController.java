faa2883e0806e826ad6b8eeb4bb10e1a1a3a9856
rajan4405 <rajanshatma652@gmail.com>
2024-05-01 09:46:01 +0545

    initialize scm project and add tailwind, flowbite

---
+++ src/main/java/com/scm/scm/controllers/PageController.java
@@ -0,0 +1,26 @@
+package com.scm.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+  @RequestMapping("/home")
+  public String home(Model model) {
+    model.addAttribute("name", "Technology");
+    model.addAttribute("GitHub", "github.com/scm-spain/scm-webapp");
+    return "home";
+  }
+
+  @RequestMapping("/about")
+  public String about() {
+    return "about";
+  }
+
+  @RequestMapping("/services")
+  public String services() {
+    return "services";
+  }
+}
